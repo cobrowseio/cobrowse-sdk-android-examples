6709c6ef74d121a70bc13478e011d29841fdcf79
Mikalai Daronin <me@mikalaidaronin.info>
2023-08-21 23:19:14 +0300

    feat: implement the new `POST_NOTIFICATIONS` permission on Android 13

--- standalone/src/main/java/io/cobrowse/standalone/MainApplication.java
+++ standalone/src/main/java/io/cobrowse/standalone/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.multidex.MultiDexApplication;
 import androidx.preference.PreferenceManager;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +29,7 @@ public class MainApplication extends MultiDexApplication
                    SharedPreferences.OnSharedPreferenceChangeListener,
                    Application.ActivityLifecycleCallbacks {
 
+    private static final String TAG = "MainApplication";
     private final List<Activity> createdActivities = new ArrayList<>();
 
     @Override
@@ -42,6 +47,13 @@ public class MainApplication extends MultiDexApplication
         CobrowseIO.instance().api(CobrowsePreferences.getApi(this));
         CobrowseIO.instance().license(CobrowsePreferences.getLicense(this));
         CobrowseIO.instance().start();
+
+        if (FirebaseApp.getApps(this).size() > 0) {
+            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(instanceIdResult
+                    -> CobrowseIO.instance().setDeviceToken(MainApplication.this, instanceIdResult.getToken()));
+        } else {
+            Log.e(TAG, "Firebase app is not initialized. Did you copy your `google-services.json` file?");
+        }
     }
 
     private void updateCobrowseConfiguration() {
