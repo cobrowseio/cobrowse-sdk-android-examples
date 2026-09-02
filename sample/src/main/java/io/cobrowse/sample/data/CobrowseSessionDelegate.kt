271c6181bafcf24f9c1c43bd1afc73cf51df2cf9
Ross Steytler <rosssteytler@gmail.com>
2026-09-02 12:41:27 +0200

    chore: custom consent prompt

--- sample/src/main/java/io/cobrowse/sample/data/CobrowseSessionDelegate.kt
+++ sample/src/main/java/io/cobrowse/sample/data/CobrowseSessionDelegate.kt
@@ -3,17 +3,21 @@ package io.cobrowse.sample.data
 import android.app.Activity
 import android.content.Context
 import android.view.View
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.preference.PreferenceManager
 import io.cobrowse.CobrowseIO
 import io.cobrowse.Session
+import io.cobrowse.sample.ui.ConsentDialogFragment
 
 /**
  * Global delegate of Cobrowse.io sessions. Stores the current session and provides a way
  * to subscribe to session data updates.
  */
-object CobrowseSessionDelegate : CobrowseIO.Delegate, CobrowseIO.RedactionDelegate {
+object CobrowseSessionDelegate : CobrowseIO.Delegate, CobrowseIO.RedactionDelegate,
+    CobrowseIO.SessionRequestDelegate, CobrowseIO.RemoteControlRequestDelegate,
+    CobrowseIO.FullDeviceRequestDelegate {
 
     private val _current = MutableLiveData<Session?>()
     val current: LiveData<Session?> = _current
@@ -24,6 +28,26 @@ object CobrowseSessionDelegate : CobrowseIO.Delegate, CobrowseIO.RedactionDelega
 
     override fun sessionDidEnd(session: Session) {
         _current.value = null
+    }
+
+    override fun handleSessionRequest(activity: Activity?, session: Session) {
+        _current.value = session
+
+        showConsent(ConsentDialogFragment.Request.Screenshare, activity)
+    }
+
+    override fun handleRemoteControlRequest(activity: Activity?, session: Session) {
+        showConsent(ConsentDialogFragment.Request.RemoteControl, activity)
+    }
+
+    override fun handleFullDeviceRequest(activity: Activity?, session: Session) {
+        showConsent(ConsentDialogFragment.Request.FullDevice, activity)
+    }
+
+    private fun showConsent(request: ConsentDialogFragment.Request, activity: Activity?) {
+        val manager = (activity as? FragmentActivity)?.supportFragmentManager ?: return
+
+        ConsentDialogFragment.show(request, manager)
     }
 
     override fun redactedViews(activity: Activity): MutableList<View> {
